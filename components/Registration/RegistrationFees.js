43047dde60ce3fa2c9662188885d1000aba0b11d
Prateek Bhatnagar <prateek89born@gmail.com>
2017-01-22 11:40:25 +0530

    reg fees module

---
+++ components/Registration/RegistrationFees.js
@@ -0,0 +1,61 @@
+import {h, Component} from 'preact';
+import {RegistrationFeesData} from '../../utils/feesUtils';
+import './RegistrationFees.css';
+
+export default class RegistrationFees extends Component {
+ render(){
+	 return (
+		 <div className='page page-regfees'>
+			<div className='content-regfees'>
+				<h2 className="typl8-delta">Registration Fees</h2>
+				<table border='1'>
+					<thead>
+                        <td>CODE</td>
+                        <td>PARTICIPANTS</td>
+                        <td>CONFERENCE</td>
+                        <td>PRE-CONFERENCE WORKSHOP</td>
+                    </thead>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <table style='width:100%'>
+                                <tr>
+                                    <td className='cell'>Early bird (till 20-1-2017)</td>
+                                    <td className='cell'>Till 20-2-2017</td>
+                                    <td className='cell'>On Spot</td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            Registration open till 10 - 2 2017
+                        </td>
+                    </tr>
+                    {
+                        RegistrationFeesData.map(item=>{
+                            return (
+                                <tr>
+                                    <td>{item.code}</td>
+                                    <td>{item.name}</td>
+                                    <td>
+                                        <table style='width:100%'>
+                                            <tr>
+                                                <td className='cell'>{item.fees[0]}</td>
+                                                <td className='cell'>{item.fees[0]}</td>
+                                                <td className='cell'>{item.fees[0]}</td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                    <td>
+                                        {item.preconffees}
+                                    </td>
+                                </tr>
+                            );
+                        })
+                    }
+				</table>
+			</div>
+		 </div>
+	 );
+ }
+}
---
+++ utils/feesUtils.js
@@ -0,0 +1,48 @@
+const RegistrationFeesData = [
+    {
+        code: 'A',
+        name: 'PROFESSIONAL',
+        fees: ['INR 4000/-','INR 5000/-','INR 6000/-'],
+        preconffees: 'INR 3000/-',
+    },
+    {
+        code: 'B',
+        name: 'STUDENTS',
+        fees: ['INR 1500/-','INR 2000/-','INR 2500/-'],
+        preconffees: 'INR 1000/-',
+    },
+    {
+        code: 'C',
+        name: 'International Professionals',
+        fees: ['USD 500/-','USD 600/-','USD 700/-'],
+        preconffees: 'USD 200',
+    },
+    {
+        code: 'D',
+        name: 'International Students',
+        fees: ['USD 200/-','USD 300/-','USD 400/-'],
+        preconffees: 'USD 100',
+    },
+    {
+        code: 'E',
+        name: 'International Professional (LMIC)',
+        fees: ['USD 300/-','USD 400/-','USD 500/-'],
+        preconffees: 'USD 100',
+    },
+    {
+        code: 'F',
+        name: 'International Students (LMIC)',
+        fees: ['USD 100/-','USD 200/-','USD 300/-'],
+        preconffees: 'USD 50',
+    },
+    {
+        code: 'F',
+        name: 'Accompanying Person',
+        fees: ['INR 1500/-, USD 100/-','INR 2000/-, USD 150/-','INR 2500/-, USD 200/-'],
+        preconffees: 'N/A',
+    },
+];
+
+export {
+    RegistrationFeesData,
+}
